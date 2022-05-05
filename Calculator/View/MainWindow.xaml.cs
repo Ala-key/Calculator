542ca8472cd9d1a535f698f1fb76608066b7f469
Альмир Абдрасулов <abdrasulov.almir@mail.ru>
2022-05-05 12:27:07 +0600

    Добавьте файлы проекта.

---
+++ Calculator/View/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in grid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+
+
+
+
+
+
+
+
+
+
+            if (str == "AC")
+            {
+                t1.Text = t1.Text.Remove(t1.Text.Length - 1);
+            }
+            else if (str == "=")
+            {
+                //Вычисление 
+                string value = new DataTable().Compute(t1.Text, null).ToString();
+                t1.Text = value;
+            }
+            else
+            {
+                t1.Text += str;
+            }
+        }
+
+       
+    }
+}
